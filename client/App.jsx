7660571b153f85840a30f39d83c25e4d921970ad
Nate Foss <nathanielfoss11@gmail.com>
2021-01-07 22:12:16 -0700

    Initial repo setup with server connected to frontend

---
+++ client/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class BowlingGame extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+
+    return (
+      <h1>Bowling Game</h1>
+    )
+  }
+};
+
+export default BowlingGame;
---
+++ client/index.jsx
@@ -0,0 +1,5 @@
+import BowlingGame from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<BowlingGame/>, document.getElementById('game'));
